5a05a43c577d4d524161603d32778d08c1cb9b3b
Mohammad Sajid <mohdsajidgwa@gmail.com>
2020-04-25 00:25:33 +0530

    Updates

---
+++ precache-manifest.cc2dbb18371f22e0c990d3fc54180e17.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7759b80e201360657c5a0ac7bd653b8",
+    "url": "/QH-Inter-College-Vision/index.html"
+  },
+  {
+    "revision": "ea9cc25008afeb1d641f",
+    "url": "/QH-Inter-College-Vision/static/css/main.28149bf3.chunk.css"
+  },
+  {
+    "revision": "d0e7b0368dd2c4e8fab6",
+    "url": "/QH-Inter-College-Vision/static/js/2.27cc0164.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/QH-Inter-College-Vision/static/js/2.27cc0164.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea9cc25008afeb1d641f",
+    "url": "/QH-Inter-College-Vision/static/js/main.8bfd1ca8.chunk.js"
+  },
+  {
+    "revision": "c2fa16d821ee6e69c84b",
+    "url": "/QH-Inter-College-Vision/static/js/runtime-main.85764345.js"
+  },
+  {
+    "revision": "2beca9de8db05d5432d41a7bd0aba6c1",
+    "url": "/QH-Inter-College-Vision/static/media/Logo.2beca9de.png"
+  },
+  {
+    "revision": "7ebbfd81890d3195b091c2a322fcbf96",
+    "url": "/QH-Inter-College-Vision/static/media/MyLogo.7ebbfd81.jpg"
+  },
+  {
+    "revision": "2e2e4b1adee30eff832e97dbb19346b0",
+    "url": "/QH-Inter-College-Vision/static/media/WhatsApp.2e2e4b1a.jpeg"
+  }
+]);
